70c9225b0f7413a360c3f9ca847ac61a15891225
Nicholas Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-11-20 16:05:27 -0500

    feat: Fix up styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { SpotifyTimeline } from "./components/Timeline"
-import { Box } from "@chakra-ui/react"
+import { ChakraProvider, Box, Heading } from "@chakra-ui/react"
 
 function App() {
   return (
-    <Box className="App" padding="2em">
-      <h1>Spotify Histogram</h1>
-      <p>
-        A timeline of the most popular songs by the years - powered by the 
-        Spotify API.
-      </p>
-      <SpotifyTimeline />
-    </Box>
+    <ChakraProvider>
+      <Box 
+        className="App" 
+        padding="2em" 
+        minH="100vh" 
+        bgGradient="linear(to-br, rgb(245, 255, 156), rgb(25, 26, 21))"
+      >
+        <Heading as="h1">Spotify Histogram</Heading>
+        <p>
+          A timeline of the most popular songs by the years - powered by the 
+          Spotify API.
+        </p>
+        <SpotifyTimeline />
+      </Box>
+    </ChakraProvider>
   );
 }
 
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,15 +1,20 @@
 import { useEffect } from "react"
 import { Box } from "@chakra-ui/react"
 
+const additionalOptions = {
+  font: "ubuntu",
+}
+
 export const SpotifyTimeline = () => {
   useEffect(() => { 
     // TODO: Replace google sheets url with custom json function  
     const timeline = new window.TL.Timeline('timeline-embed',
-            'https://docs.google.com/spreadsheets/d/1cWqQBZCkX9GpzFtxCWHoqFXCHg-ylTVUWlnrdYMzKUI/pubhtml');
+            'https://docs.google.com/spreadsheets/d/1cWqQBZCkX9GpzFtxCWHoqFXCHg-ylTVUWlnrdYMzKUI/pubhtml',
+            additionalOptions);
   })
   
   return (
-    <Box width="100%" height="40vw" maxW="1600px">
+    <Box width="100%" maxW="1600px" height="40vw" paddingTop="1em" borderRadius="8px">
       <Box id="timeline-embed"/>
     </Box>
   )
