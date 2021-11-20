0052b30c3cc29cd6d6e56ece6cc96334f69865f1
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2021-11-20 13:14:25 -0500

    Feat: Implement getWikipediaTable()

---
+++ src/script.js
@@ -0,0 +1,26 @@
+var axios = require("axios");
+
+function getWikipediaTable() {
+  var config = {
+    method: "get",
+    url: "https://www.wikitable2json.com/api/List_of_most-streamed_songs_on_Spotify",
+    headers: {},
+  };
+
+  axios(config)
+    .then(function (response) {
+      const firstTable = response.data[0].data;
+      for (var i = 0; i < firstTable.length; i++) {
+        const entry = firstTable[i];
+        if (!isNaN(entry[0])) {
+          console.log(entry[1]);
+        }
+      }
+      //   console.log(response.data[0].data[1][1])
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+getWikipediaTable();
