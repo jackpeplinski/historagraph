4c836ef36b1812385f655e34739de33013c61be9
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2021-11-20 17:59:23 -0500

    Feat: Implement more methods for JSON

--- src/script.js
+++ src/script.js
@@ -11,14 +11,109 @@ function getWikipediaTable() {
     .then(function (response) {
       const firstTable = response.data[0].data;
       for (var i = 0; i < firstTable.length; i++) {
+        /*
+            entry = [
+                "1",
+                "\"Shape of You\"",
+                "2,971",
+                "Ed Sheeran",
+                "÷",
+                "6 January 2017",
+                "[11]"
+            ],
+        */
         const entry = firstTable[i];
         if (!isNaN(entry[0])) {
-          console.log(entry[1]);
+          // if the song doesn't have a rank, the entry is not a song so it can be ignored
+          //   console.log(entry[1]);
+
+          getSpotifyID(entry[1]);
           // for each title search spotify for track id
           // for each track id get the release date
+          //   getGeniusID(entry[1].replace(/['"]+/g, ""));
         }
       }
-      //   console.log(response.data[0].data[1][1])
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function getSpotifyID(trackName) {
+  var config = {
+    method: "get",
+    url: `https://api.spotify.com/v1/search?q=${trackName}&type=track`,
+    headers: {
+      Authorization:
+        "Bearer BQA3gupRatv_SUmGfkmO1mj4dyE4T_DoVOpw3NC3l3bx98qbdySpcIBAKmFG7y6TqQ_gOmTBayJ686R0FeU",
+      "Content-Type": "application/json",
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(response.data.tracks.items[0].id);
+      getReleaseDate(response.data.tracks.items[0].id);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function getReleaseDate(trackID) {
+  var config = {
+    method: "get",
+    url: `https://api.spotify.com/v1/tracks/${trackID}`,
+    headers: {
+      Authorization:
+        "Bearer BQA3gupRatv_SUmGfkmO1mj4dyE4T_DoVOpw3NC3l3bx98qbdySpcIBAKmFG7y6TqQ_gOmTBayJ686R0FeU",
+      "Content-Type": "application/json",
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(response.data.album.release_date);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function getGeniusID(trackName) {
+  var config = {
+    method: "get",
+    url: `https://api.genius.com/search?q=${trackName}`,
+    headers: {
+      Authorization:
+        "Bearer D6Ek2_F2P1-2vOCr7jeaU9Cuvec7Vmdof-IIp6y0Uzv26DRu6n4vrQB_ujK35h4R",
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(response.data.response.hits[0].result.api_path);
+      getDescription(response.data.response.hits[0].result.api_path);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function getDescription(apiPath) {
+  var config = {
+    method: "get",
+    url: `https://api.genius.com${apiPath}?text_format=plain`,
+    headers: {
+      Authorization:
+        "Bearer D6Ek2_F2P1-2vOCr7jeaU9Cuvec7Vmdof-IIp6y0Uzv26DRu6n4vrQB_ujK35h4R",
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(response.data.response.song.description.plain);
+      //   console.log(response.data.response.description.plain);
     })
     .catch(function (error) {
       console.log(error);
@@ -26,3 +121,4 @@ function getWikipediaTable() {
 }
 
 getWikipediaTable();
+// getReleaseDate("1zB4vmk8tFRmM9UULNzbLB")
