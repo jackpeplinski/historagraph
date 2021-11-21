c6199e1ef39e2f7f74d14f3e2636b5b379dae14d
Nicholas Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-11-20 22:26:55 -0500

    refactor: Change all functions to async/await

--- src/script.js
+++ src/script.js
@@ -1,66 +1,62 @@
 var axios = require("axios");
 var fs = require("fs");
 
-function getWikipediaTable() {
+async function getWikipediaTable() {
   var config = {
     method: "get",
     url: "https://www.wikitable2json.com/api/List_of_most-streamed_songs_on_Spotify",
     headers: {},
   };
 
-  axios(config)
-    .then(function (response) {
-      const firstTable = response.data[0].data;
-      for (var i = 0; i < firstTable.length; i++) {
-        /*
-            entry = [
-                "1",
-                "\"Shape of You\"",
-                "2,971",
-                "Ed Sheeran",
-                "÷",
-                "6 January 2017",
-                "[11]"
-            ],
-        */
-        const entry = firstTable[i];
-        if (!isNaN(entry[0])) {
-          // if the song doesn't have a rank, the entry is not a song so it can be ignored
-          getSpotifyData(entry[1]);
-          // for each title search spotify for track id
-          // for each track id get the release date
-          //   getGeniusID(entry[1].replace(/['"]+/g, ""));
-        }
+  const response = await axios(config)
+  const firstTable = response.data[0].data;
+  for (var i = 0; i < firstTable.length; i++) {
+    /*
+        entry = [
+            "1",
+            "\"Shape of You\"",
+            "2,971",
+            "Ed Sheeran",
+            "÷",
+            "6 January 2017",
+            "[11]"
+        ],
+    */
+    const entry = firstTable[i];
+    if (!isNaN(entry[0])) {
+      // if the song doesn't have a rank, the entry is not a song so it can be ignored
+      try {
+        await getSpotifyData(entry[1]);
+      } catch (e) {
+        return console.log(i)
       }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      // for each title search spotify for track id
+      // for each track id get the release date
+      //   getGeniusID(entry[1].replace(/['"]+/g, ""));
+    }
+  }
+
+  return true
 }
 
-function getSpotifyData(trackName) {
+async function getSpotifyData(trackName) {
   var config = {
     method: "get",
     url: `https://api.spotify.com/v1/search?q=${trackName}&type=track`,
     headers: {
       Authorization:
-        "Bearer BQBTrx-obX7qQm4lAZ55p1Sa6_iUocP6w53ZfVmHJUbtFGQ53ZzF0O9MfHKxqpyET0gfvdcnVAAkgfP_xd0",
+        "Bearer BQCgbkW-kh_Qmu0wW4fH54WqRWPbFuVb_wjEd-hMyZgCoqt1pVb7_fnVPCCsVE3ysaLl-22QEvmG4CUARTg",
       "Content-Type": "application/json",
     },
   };
 
-  axios(config)
-    .then(function (response) {
-      const id = response.data.tracks.items[0].id;
-      const releaseDate = response.data.tracks.items[0].album.release_date;
-      getGeniusID(trackName, releaseDate, id);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  const response = await axios(config)
+  const id = response.data.tracks.items[0].id;
+  const releaseDate = response.data.tracks.items[0].album.release_date;
+  return getGeniusID(trackName, releaseDate, id);
 }
 
-function getGeniusID(trackName, releaseDate, id) {
+async function getGeniusID(trackName, releaseDate, id) {
   var config = {
     method: "get",
     url: `https://api.genius.com/search?q=${trackName}`,
@@ -70,14 +66,9 @@ function getGeniusID(trackName, releaseDate, id) {
     },
   };
 
-  axios(config)
-    .then(function (response) {
-      const apiPath = response.data.response.hits[0].result.api_path;
-      getDescription(apiPath, trackName, releaseDate, id);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  const response = await axios(config)
+  const apiPath = response.data.response.hits[0].result.api_path;
+  return getDescription(apiPath, trackName, releaseDate, id);  
 }
 
 var i = 0;
@@ -101,7 +92,7 @@ const json = {
     */
   ],
 };
-function getDescription(apiPath, trackName, releaseDate, id) {
+async function getDescription(apiPath, trackName, releaseDate, id) {
   var config = {
     method: "get",
     url: `https://api.genius.com${apiPath}?text_format=plain`,
@@ -111,35 +102,59 @@ function getDescription(apiPath, trackName, releaseDate, id) {
     },
   };
 
-  axios(config)
-    .then(function (response) {
-      const description = response.data.response.song.description.plain;
-      //   console.log(
-      //     `TRACKNAME: ${trackName}, ID:${id}, RELEASEDATE: ${releaseDate}, DESCRIPTION: ${description}`
-      //   );
-      const splitReleaseDate = releaseDate.split("-");
+  const response = await axios(config)
+  const description = response.data.response.song.description.plain;
+  //   console.log(
+  //     `TRACKNAME: ${trackName}, ID:${id}, RELEASEDATE: ${releaseDate}, DESCRIPTION: ${description}`
+  //   );
+  const splitReleaseDate = releaseDate.split("-");
 
-      const event = {
-        media: {
-          url: `https://open.spotify.com/track/${id}`,
-        },
-        text: {
-          headline: trackName,
-          text: description,
-        },
-        start_date: {
-          month: splitReleaseDate[1],
-          day: splitReleaseDate[2],
-          year: splitReleaseDate[0],
-        },
-      };
+  const event = {
+    media: {
+      url: `https://open.spotify.com/track/${id}`,
+    },
+    text: {
+      headline: trackName,
+      text: description,
+    },
+    start_date: {
+      month: splitReleaseDate[1],
+      day: splitReleaseDate[2],
+      year: splitReleaseDate[0],
+    },
+  };
 
-      json.events.push(event);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  json.events.push(event);
+  return true
 }
 
-getWikipediaTable();
-// getReleaseDate("1zB4vmk8tFRmM9UULNzbLB")
+async function makeJson() {
+  await getWikipediaTable()
+  fs.writeFile("songs.js", JSON.stringify(json, null, 2), (err) => {
+    console.log("JSON data is saved.")
+  })
+  return true
+}
+
+function getNewToken() {
+  var config = {
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token?grant_type=client_credentials',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Authorization': 'Basic M2UxN2JhOGVlYmZjNDU1ZmIyOWQ0NTA3OTAzZTJmYmI6YTQxMjdmMzAzMWUzNGU2ZmE0MTc0MzZiNmI3MGRhOTY=', 
+      'Cookie': '__Host-device_id=AQAXLY-ZTeaVSbhFogtn8i-yBlPlI4SX-jwehnCOy9Q6GsBSVimSCVmqfGcthT89vLQqbSGFUvXPS2d-8wvdZZxgL-1EKYFpeKk; sp_tr=false'
+    }
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+// getNewToken()
+makeJson()
