4599d8a3fa7a5286840e795605c1aafb0e0f64f0
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2021-11-20 19:12:41 -0500

    Feat: Create JSON object

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -21,6 +21,20 @@ export const SpotifyTimeline = () => {
           day: "3",
           year: "2012",
         },
+      },
+      {
+        media: {
+          url: "https://open.spotify.com/track/0uI7yAKUf52Cn7y3sYyjiX?si=93af3eb5afbc414e"
+        },
+        text: {
+          headline: "3 Nights: Dominic Fike",
+          text: "\"3 Nights\" is the debut single by American singer and rapper Dominic Fike. It was released as a single from his Don't Forget About Me, Demos EP through Columbia Records on October 16, 2018, and peaked within the top ten of the charts in Australia, Belgium (Flanders), Croatia, Israel, the Netherlands, New Zealand, the Republic of Ireland and the United Kingdom, and within the top forty of the charts in Germany. \"3 Nights\" revolves around Fike staying up late at a motel in the \"City of Palms <a href=\"http://www.last.fm/music/Dominic+Fike/_/3+Nights\">Read more on Last.fm</a>.",
+        },
+        start_date: {
+          month: "1",
+          day: "3",
+          year: "2012",
+        },
       }
     ],
   };
--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 var axios = require("axios");
+var fs = require("fs");
 
 function getWikipediaTable() {
   var config = {
@@ -25,7 +26,6 @@ function getWikipediaTable() {
         const entry = firstTable[i];
         if (!isNaN(entry[0])) {
           // if the song doesn't have a rank, the entry is not a song so it can be ignored
-          console.log(entry[1]);
           getSpotifyID(entry[1]);
           // for each title search spotify for track id
           // for each track id get the release date
@@ -51,15 +51,16 @@ function getSpotifyID(trackName) {
 
   axios(config)
     .then(function (response) {
-      console.log(response.data.tracks.items[0].id);
-      console.log(response.data.tracks.items[0].album.release_date);
+      const id = response.data.tracks.items[0].id;
+      const releaseDate = response.data.tracks.items[0].album.release_date;
+      getGeniusID(trackName, releaseDate, id);
     })
     .catch(function (error) {
       console.log(error);
     });
 }
 
-function getGeniusID(trackName) {
+function getGeniusID(trackName, releaseDate, id) {
   var config = {
     method: "get",
     url: `https://api.genius.com/search?q=${trackName}`,
@@ -71,15 +72,36 @@ function getGeniusID(trackName) {
 
   axios(config)
     .then(function (response) {
-      console.log(response.data.response.hits[0].result.api_path);
-      getDescription(response.data.response.hits[0].result.api_path);
+      const apiPath = response.data.response.hits[0].result.api_path;
+      getDescription(apiPath, trackName, releaseDate, id);
     })
     .catch(function (error) {
       console.log(error);
     });
 }
 
-function getDescription(apiPath) {
+var i = 0;
+const json = {
+  events: [
+    /*
+    {
+      media: {
+        url: "https://open.spotify.com/track/0uI7yAKUf52Cn7y3sYyjiX?si=93af3eb5afbc414e",
+      },
+      text: {
+        headline: "3 Nights: Dominic Fike",
+        text: '"3 Nights" is the debut single by American singer and rapper Dominic Fike. It was released as a single from his Don\'t Forget About Me, Demos EP through Columbia Records on October 16, 2018, and peaked within the top ten of the charts in Australia, Belgium (Flanders), Croatia, Israel, the Netherlands, New Zealand, the Republic of Ireland and the United Kingdom, and within the top forty of the charts in Germany. "3 Nights" revolves around Fike staying up late at a motel in the "City of Palms <a href="http://www.last.fm/music/Dominic+Fike/_/3+Nights">Read more on Last.fm</a>.',
+      },
+      start_date: {
+        month: "1",
+        day: "3",
+        year: "2012",
+      },
+    },
+    */
+  ],
+};
+function getDescription(apiPath, trackName, releaseDate, id) {
   var config = {
     method: "get",
     url: `https://api.genius.com${apiPath}?text_format=plain`,
@@ -91,8 +113,28 @@ function getDescription(apiPath) {
 
   axios(config)
     .then(function (response) {
-      console.log(response.data.response.song.description.plain);
-      //   console.log(response.data.response.description.plain);
+      const description = response.data.response.song.description.plain;
+      //   console.log(
+      //     `TRACKNAME: ${trackName}, ID:${id}, RELEASEDATE: ${releaseDate}, DESCRIPTION: ${description}`
+      //   );
+      const splitReleaseDate = releaseDate.split("-");
+
+      const event = {
+        media: {
+          url: `https://open.spotify.com/track/${id}`,
+        },
+        text: {
+          headline: trackName,
+          text: description,
+        },
+        start_date: {
+          month: splitReleaseDate[1],
+          day: splitReleaseDate[2],
+          year: splitReleaseDate[0],
+        },
+      };
+
+      json.events.push(event);
     })
     .catch(function (error) {
       console.log(error);
