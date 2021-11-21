cb2518eedb0fad7f77ea12229346876983ceb5dc
Nicholas Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-11-20 22:27:13 -0500

    chore: Run node script.js to create songs.js file

---
+++ src/songs.js
@@ -0,0 +1,116 @@
+export const songs = {
+  "events": [
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/7qiZfU4dY1lWllzX7mPBI3"
+      },
+      "text": {
+        "headline": "\"Shape of You\"",
+        "text": "Ed Sheeran released “Shape of You” together with “Castle On The Hill.” The two songs were the first releases from his third album, ÷ (Divide), following in the mathematical footsteps of his +  (Plus) and x (Multiply) albums.\n\n“Shape Of You” is based around a tropical house-inspired lead melody and dancehall beat. A rhythm is given by xylophones, guitars, and percussions. The style and rhythm have been noted as close to the  Sia successes, “Cheap Thrills”, which has caused some controversy. Ed has denied receiving inspiration from the track. Just like “Cheap Thrills”, however, “Shape of You” was written with the intention of it being recorded by Rihanna, but Sheeran later decided to keep it for himself.\n\nIn the song, Sheeran sings about meeting someone at a bar and falling in love with her; a love distinct from those in much of his previous work as it’s more focused on the physical than the emotional.\n\nSheeran had teased the song through a tweet that he had posted on January 5, 2017. The video had a colon that would then merge into a human body, referencing the “body”-inspiration of the song.\n\nAs of August 29, 2017, “Shape Of You” set a Billboard record, spending 33 straight weeks in the Top 10 of the Hot 100 chart.\n\nOn September 22, 2017, the track became the most streamed song of all time on the Spotify plaform at 1.318 billion plays. Sheeran passed Drake who previously held the record with “One Dance.”\n\nThe song won a Grammy Best Pop Solo Performance at the 2018 Grammys held on January 28, 2018.\n\nOn January 24th, 2019, “Shape Of You” was certified Diamond by the RIAA, selling over ten million units."
+      },
+      "start_date": {
+        "month": "03",
+        "day": "03",
+        "year": "2017"
+      }
+    },
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b"
+      },
+      "text": {
+        "headline": "\"Blinding Lights\"",
+        "text": "“Blinding Lights” is the second single to The Weeknd’s fourth studio album After Hours, following the record’s lead single “Heartless,” which released two days prior on November 27, 2019. The track finds Abel in a constant state of distraction that he only gets relief from when in the presence of a significant other, as he sings over an up-tempo electropop instrumental that features large ‘80s-inspired synths and synthwave drums, similar to the sonic direction of his third studio album, Starboy.\n\nThe track was first announced on November 24, 2019 in the trailer for a Mercedes Benz advertisement. The full commercial debuted minutes after the song’s release and is heavily centered around it. In the ad, The Weeknd drives around in a Mercedes car as the track plays in the background.\n\nThe song leaked on November 26, 2019, a few days before its official release. Abel acknowledged this in a tweet, where he confirmed its official release:\n\noh yah… and more new music tonight (in HQ😉)\n\nA music video for the track was released on January 21, 2020. The video is a continuation of the music video from “Heartless,” starting with Abel stopping his manic run through Las Vegas, which is where the video for “Heartless” ended. From there, Abel continues his tear through the city while under the influence of many substances, this time going to a strip club as well as driving a different luxury Mercedes Benz AMG-GT in the video."
+      },
+      "start_date": {
+        "month": "03",
+        "day": "20",
+        "year": "2020"
+      }
+    },
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/2XU0oxnq2qxCpomAAuJY8K"
+      },
+      "text": {
+        "headline": "\"Dance Monkey\"",
+        "text": "“Dance Monkey” is a song by Australian singer Tones and I, released as the second single from her debut EP The Kids Are Coming. In the track, Tones sings about the struggles of being a musical performer, inspired by her experiences as a busker. She explained to Billboard:\n\nIt’s a song about a moment where I felt frustrated about how people had no patience anymore. […] If you replace the word ‘dance’ with ‘sing,’ it’s pretty self-explanatory. I’ve literally turned quotes that people say to me into lyrics – they’re all very real. […] It was one night where people just wouldn’t let me be. I’d been busking for seven or eight hours, and I had done six encores. So many people get inspired in the moment and just make up bullshit to try to give you hope. Every person swears black and blue on their kids’ lives that they’re someone and they can help me and they never do.\n\n“Dance Monkey” was written in October 2018, at her manager’s house. Tones posted a video of herself performing the track the day she wrote it."
+      },
+      "start_date": {
+        "month": "10",
+        "day": "17",
+        "year": "2019"
+      }
+    },
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/7ytR5pFWmSjzHJIeQkgog4"
+      },
+      "text": {
+        "headline": "\"Rockstar\"",
+        "text": "On “rockstar,” Post Malone compares his habits to the “live fast, die young” rock and roll lifestyle. Along the way, he references real events in rock history, name-drops Bon Scott and Jim Morrison, and highlights rockstar traditions like throwing TVs out of hotel windows.\n\nPost previewed the song in December 2016, nine months before he released it, by posting a video of himself jamming to it in the studio. The preview received thousands of retweets on Twitter over the course of the year, so the track was highly anticipated by the time it finally dropped.\n\nIn late December of 2017, it was also revealed that Joey Bada$$ had co-written the track as well as penning an extra verse after Music Mafia leaked the original version of the song. This version also contained a verse from T-Pain, which he confirmed, in place of the 21 Savage’s contributions that ultimately appeared on the final version of the song.\n\nOn January 9, 2018, “rockstar” broke the record for holding the number one spot on Spotify’s Global chart with 108 days spent on the top of the list. The previous record holder was Ed Sheeran’s “Shape Of You.”"
+      },
+      "start_date": {
+        "month": "04",
+        "day": "17",
+        "year": "2020"
+      }
+    },
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/1zi7xx7UVEFkmKfv06H8x0"
+      },
+      "text": {
+        "headline": "\"One Dance\"",
+        "text": "“One Dance” is an R&B song with dancehall and Afrobeats inflections, featuring a slowed down sample of the vocals and chord stabs from a 2008 UK Funky house anthem, Kyla’s “Do You Mind,” produced by Crazy Cousinz.\n\n“One Dance” also features Nigerian singer Wizkid—he and Drake teamed up in 2015 to remix his track “Ojuelegba.”\n\nSpotify released their list of most-streamed songs of summer 2016—“One Dance” took the number one spot on the US and Global charts. On October 17, 2016, “One Dance” became the number one streamed song in the streaming service’s history."
+      },
+      "start_date": {
+        "month": "05",
+        "day": "06",
+        "year": "2016"
+      }
+    },
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/7qEHsqek33rTcFNT9PFqLf"
+      },
+      "text": {
+        "headline": "\"Someone You Loved\"",
+        "text": "During his speech at The BRIT Awards 2020, Lewis confirmed that contrary to popular belief, this song is about his grandmother (as opposed to an ex-lover).\n\nThe music video, starring the former Doctor Who star  Peter Capaldi, depicts him as a widower. He is content, but when he meets a young mother and her family, who his wife had donated a heart to, and he gets to listen to her heartbeat again.\n\n“Someone You Loved” was Lewis' first UK number 1 single and spent seven consecutive weeks at the top. It also hit #1 on Scotland and Ireland’s charts."
+      },
+      "start_date": {
+        "month": "05",
+        "day": "17",
+        "year": "2019"
+      }
+    },
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/7BKLCZ1jbUBVqRi2FVlTVw"
+      },
+      "text": {
+        "headline": "\"Closer\"",
+        "text": "“Closer” is a millennial romance anthem that celebrates youth and heartbreak. It features vocals from singer-songwriter Halsey and Chainsmokers member Andrew Taggart, marking the first time the Chainsmokers feature their vocals on a track—Taggart previously performed backing vocals for the chorus of “Roses”. It is also their first collaboration with Halsey.\n\nAt the 2016 MTV VMAs, both the duo and Halsey performed “Closer”. The duo teased the performance as “new level” on Twitter. This was the second time that “Closer” was performed at Madison Square Garden — the first being at Halsey’s sold-out Badlands Tour show on August 13th, 2016 where The Chainsmokers performed as guests.\n\nhttps://www.youtube.com/watch?v=agFMqNB9BYM\n\nIn September 2016, after “Closer” had already reached the summit of the Billboard Hot 100, Isaac Slade and Joe King, respectively the lead singer and guitarist of The Fray, were given additional songwriting credits on the song due to the similarity of its chorus to the verses of their 2005 breakout hit, “Over My Head (Cable Car).”\n\nOn the March-25-dated chart, “Closer” tied LeAnn Rimes Cibrian’s 32 week record in the Billboard Hot 100 top 10. The singer held it for 20 years – beginning December 13, 1997 – with her single “How Do I Live.” She congratulated the hitmaker duo on Twitter by singing a mashup of “Closer” and her own song.\n\nOn September 15, 2018, the song was certified Diamond by the RCAA, verifying that the song had sold an equivalent ten million units in the United States alone.\n\nRelated: How Blink 182’s “I Miss You” Inspired The Chainsmokers And Halsey’s “Closer”"
+      },
+      "start_date": {
+        "month": "07",
+        "day": "29",
+        "year": "2016"
+      }
+    },
+    {
+      "media": {
+        "url": "https://open.spotify.com/track/3KkXRkHbMCARz0aVfEt68P"
+      },
+      "text": {
+        "headline": "\"Sunflower\"",
+        "text": "“Sunflower” marks the second collaboration by Post Malone and Swae Lee. It serves as the second single for Post Malone’s 2019 album, Hollywood’s Bleeding, and is part of the soundtrack for the 2018 animated film Spider-Man: Into the Spider-Verse. The track would end up breaking the record for most weeks spent in the top 10 of the Billboard Hot R&B/Hip-Hop Songs Chart after notching its 45th week.\n\nSunflowers are a symbol of loyalty and longevity because of the flower’s ability to retain its beauty in the face of environmental factors that would leave other flowers wilted. The sunflower in this song represents a girl—a woman who’s shown persistence, despite a lack of affection shown to her. However, sunflowers also deplete nutrients from soil much quicker than other plants and can inhibit the growth of other plants. Post Malone’s verses could imply that the person he’s with is in constant need of help and attention, yet he is unable to tend to them as much as they would like.\n\nThis mystery girl ends up being the subject matter for both Post Malone and Swae Lee. In their verses, the girl seems to be trying much harder in the relationship than either of the two stars. However, even after all of the poor treatment the girl has received, she remains by Post and Swae’s side.\n\nPost first teased the song at The Tonight Show Starring Jimmy Fallon on October 1, 2018. He then confirmed on Facebook that the song would include Swae Lee. Two snippets from the lyric video were shown on both of their Instagram accounts on October 15, 2018."
+      },
+      "start_date": {
+        "month": "12",
+        "day": "14",
+        "year": "2018"
+      }
+    }
+  ]
+}
